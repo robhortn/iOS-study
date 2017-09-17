5577c1287ee16815befa7edb83514311deae7431
Rob Horton <robhorton@outlook.com>
2017-09-17 15:01:55 -0500

    Finishing up chp3

---
+++ iOS10Swift3Book/chp3/programming-and-vars.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Operators
+// There are three types of operators in Swift:
+
+// Unary:   Only affect one target.
+// Binary:  Operate on two targets.
+// Ternary: Handle three targets.
+
+var bankAccountBalance = 100
+var cashRegisterMessage = "You are broke as a joke"
+var itemPrice = 60
+
+print(bankAccountBalance >= itemPrice ? "Item purchased!" : cashRegisterMessage)
+
+var myName: String = "Rob Horton"
+var myAge: Int = 47
+var currentTemp: Double = 74
+var enjoySwiftProgramming: Bool = true
+var doLikeSwift = enjoySwiftProgramming == true ? "YES!" : "not really. :("
+
+var message: String = "My name is \(myName). I am \(myAge) years old. Where I live, it is currently \(currentTemp) degrees outside.  If you were to ask me if I enjoy programming in Swift I would have to say, \(doLikeSwift)"
+
+print(message)
+
+
+
