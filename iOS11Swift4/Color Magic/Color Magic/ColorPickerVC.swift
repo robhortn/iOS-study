5d36b6eadd7ce6d3bb9e902f0b83a95189bbc488
Rob Horton <robhorton@outlook.com>
2017-10-08 20:44:23 -0500

    Adding IBOutlet

---
+++ iOS11Swift4/Color Magic/Color Magic/ColorPickerVC.swift
@@ -0,0 +1,18 @@
+//
+//  ColorPickerVC.swift
+//  Color Magic
+//
+//  Created by Rob Horton on 10/8/17.
+//  Copyright © 2017 Rob Horton. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
--- iOS11Swift4/Color Magic/Color Magic/ColorPresenterVC.swift
+++ iOS11Swift4/Color Magic/Color Magic/ColorPresenterVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ColorPresenterVC.swift
 //  Color Magic
 //
 //  Created by Rob Horton on 10/8/17.
@@ -8,18 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ColorPresenterVC: UIViewController {
+
+    @IBOutlet weak var colorNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
